cc9adb431e62f14f80086d23ee2ff2359366e85e
SWCL0 <stan.w.c@icloud.com>
2024-02-08 10:32:27 +0100

    Update cirkel.php
    Toevoeging van opdracht:
    - Opvraag diameter
    - Berekenen variabelen
    - Terug geven van berekende info

--- cirkel.php
+++ cirkel.php
@@ -1,5 +1,15 @@
 <?php
 
+$pi = pi();
 
+echo "Wat is de diameter van de cirkel?" . PHP_EOL;
+$diameter = readline();
+
+$straal = $diameter / 2;
+$omtrek = $pi * $diameter;
+$oppervlakte = ($straal * $straal) * $pi;
+
+echo "Oppervlakte = " . $oppervlakte . PHP_EOL;
+echo "Omtrek = " . $omtrek . PHP_EOL;
 
 ?>
